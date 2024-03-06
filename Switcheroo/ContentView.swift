8d2c8afe17cc268e5c99091fcb07b4f23a017008
Zhenyi Tan <zhenyi@andadinosaur.com>
2024-03-07 02:53:37 +0800

    Initial Commit

---
+++ Switcheroo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Switcheroo
+//
+//  Created by Zhenyi Tan on 7/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Switcheroo/SwitcherooApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitcherooApp.swift
+//  Switcheroo
+//
+//  Created by Zhenyi Tan on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwitcherooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
