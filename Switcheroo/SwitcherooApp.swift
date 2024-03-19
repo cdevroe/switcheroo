d8810e24c8fd59f38c4f93426134e48db5691594
Colin Devroe <colin@cdevroe.com>
2024-03-19 08:00:21 -0400

    Initial commit.

--- Switcheroo/SwitcherooApp.swift
+++ Switcheroo/SwitcherooApp.swift
@@ -7,11 +7,26 @@
 
 import SwiftUI
 
+
 @main
 struct SwitcherooApp: App {
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .frame(width: 320)
+        }
+        .windowResizabilityContentSize()
+    }
+
+}
+
+extension Scene {
+    func windowResizabilityContentSize() -> some Scene {
+        if #available(macOS 13.0, *) {
+            return windowResizability(.contentSize)
+        } else {
+            return self
         }
     }
 }
